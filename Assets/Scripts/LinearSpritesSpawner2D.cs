c1f601e847b5f222f2c641f80070fd3a573da039
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-02 22:22:24 +0200

    Use Vector2 instead of Vector3

--- Assets/Scripts/LinearSpritesSpawner2D.cs
+++ Assets/Scripts/LinearSpritesSpawner2D.cs
@@ -10,11 +10,11 @@ public class LinearSpritesSpawner2D : MonoBehaviour {
     // Use this for spawning
     void Start()
     {
-        Vector3 spritePosition = new Vector3(startPositionX, StartPositionY, 0f);
+        Vector2 spritePosition = new Vector2(startPositionX, StartPositionY);
         for (int i = 0; i < 3; i++)
         {
             GameObject.Instantiate(spriteToDuplicate, spritePosition, Quaternion.identity);
-            spritePosition += new Vector3(4f, StartPositionY, 0f);
+            spritePosition += new Vector2(4f, StartPositionY);
         }
     }
 }
