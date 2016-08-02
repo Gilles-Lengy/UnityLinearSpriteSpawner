9391bd5da56efe7ebe215a94af7dcc7e03e9fd51
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-02 22:11:44 +0200

    The sprite to duplicate is now in the top left corner and the script has x position and a y position to start

--- Assets/Scripts/LinearSpritesSpawner2D.cs
+++ Assets/Scripts/LinearSpritesSpawner2D.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class LinearSpritesSpawner2D : MonoBehaviour {
 
     public GameObject spriteToDuplicate;
+    public float startPositionX;
+    public float StartPositionY;
 
     // Use this for spawning
     void Start()
     {
-        Vector3 currentPosition = spriteToDuplicate.transform.position;
+        Vector3 spritePosition = new Vector3(startPositionX, StartPositionY, 0f);
         for (int i = 0; i < 3; i++)
         {
-            GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity);
-            currentPosition += new Vector3(4f, 0f, 0f);
+            GameObject.Instantiate(spriteToDuplicate, spritePosition, Quaternion.identity);
+            spritePosition += new Vector3(4f, StartPositionY, 0f);
         }
     }
 }
