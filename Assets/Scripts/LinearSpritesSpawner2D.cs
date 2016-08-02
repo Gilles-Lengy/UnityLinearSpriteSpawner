82bbbbfc3d7ed4b1faa0ccf6a8b035bd7d7f0b46
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-02 17:34:25 +0200

    Basic script that spawn sprites created and added to an empty game object with the sprite as parameter

---
+++ Assets/Scripts/LinearSpritesSpawner2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearSpritesSpawner2D : MonoBehaviour {
+
+    public GameObject spriteToDuplicate;
+
+    // Use this for spawning
+    void Start()
+    {
+        Vector3 currentPosition = spriteToDuplicate.transform.position;
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject.Instantiate(spriteToDuplicate, currentPosition, Quaternion.identity);
+            currentPosition += new Vector3(4f, 0f, 0f);
+        }
+    }
+}
