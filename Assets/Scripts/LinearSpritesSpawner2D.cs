22c154ad172d000ba267d2351b05e4a8e8360e9d
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-02 22:44:03 +0200

    Comments added

--- Assets/Scripts/LinearSpritesSpawner2D.cs
+++ Assets/Scripts/LinearSpritesSpawner2D.cs
@@ -13,12 +13,15 @@ public class LinearSpritesSpawner2D : MonoBehaviour {
     // Use this for spawning
     void Start()
     {
+        // The position of the first generated sprite
         Vector2 spritePosition = new Vector2(startPositionX, StartPositionY);
+        // Spawning the sprites
         for (int i = 0; i < iteration; i++)
         {
             GameObject.Instantiate(spriteToDuplicate, spritePosition, Quaternion.identity);
             spritePosition += new Vector2(gapAnchorX, gapAnchorY);
         }
+        // Destroying the original sprite
         Destroy(spriteToDuplicate);
     }
 }
